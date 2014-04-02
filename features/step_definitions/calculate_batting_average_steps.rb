87bbb1ba4f999dd457c0d0c1dfc7a0efdb58c4f1
Joe Sak <joe@joesak.com>
2014-04-02 13:06:33 -0400

    Clean up step definitions, explain some things in comments

--- features/step_definitions/calculate_batting_average_steps.rb
+++ features/step_definitions/calculate_batting_average_steps.rb
@@ -2,24 +2,31 @@ Given(/^that no stats exist$/) do
 end
 
 Given(/^(?:that )?some (?:stats|at-bats) exist$/) do
-  @stats_csv = "playerID,yearID,league,teamID,G,AB,R,H,2B,3B,HR,RBI,SB,CS\naardsda01,2009,AL,SEA,73,0,0,2,0,0,0,0,0,0"
+  @stats_csv = "playerID,yearID,league,teamID,G,AB,R,H,2B,3B,HR,RBI,SB,CS\n"
+  @stats_csv << "aardsda01,2009,AL,SEA,73,0,0,2,0,0,0,0,0,0"
+  # 01 has 2 hits
 end
 
 Given(/^(?:that )?enough at\-bats exist for multiple players$/) do
   step "some at-bats exist"
   @stats_csv << "\naardsda02,2009,AL,SEA,73,0,0,1,0,0,0,0,0,0"
+  # 02 has 1 hit
   step "set at-bats to 200"
+  # everyone has 200 At-Bats
 end
 
 Given(/^that enough at\-bats exist for multiple players out of date range$/) do
   step "enough at-bats exist for multiple players"
   @stats_csv << "\naardsda03,2007,AL,SEA,73,200,0,3,0,0,0,0,0,0"
+  # 03 has 3 hits, but is out of date range
 end
 
 Given(/^that enough at\-bats exist for multiple players in date range$/) do
   step "enough at-bats exist for multiple players"
   @stats_csv << "\naardsda02,2010,AL,SEA,73,200,0,2,0,0,0,0,0,0"
   @stats_csv << "\naardsda01,2010,AL,SEA,73,200,0,2,0,0,0,0,0,0"
+  # 02 improved by 1 hit
+  # 01 did not improve, stayed the same
 end
 
 Then(/^calling calculate should return the most improved player$/) do
@@ -36,8 +43,7 @@ When(/^I create an improved batting average calculator$/) do
   csv = @stats_csv.blank? ? '' : CSV.parse(@stats_csv,
                                            headers: true,
                                            converters: :all)
-  @calculator = BaseballStats::Calculators::ImprovedBattingAverage.new(csv,
-                                                                          2010)
+  @calc = BaseballStats::Calculators::ImprovedBattingAverage.new(csv, 2010)
 end
 
 Then(/^I should see there aren't enough at-bats to calculate$/) do
@@ -71,7 +77,7 @@ end
 
 #private steps
 When(/^calculate best batting average$/) do
-  @calculator.calculate
+  @calc.calculate
 end
 
 When(/^set at-bats to (\d+)$/) do |num|
