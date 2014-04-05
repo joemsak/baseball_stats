339753967ccfe63efc9804d66ea0e0afc1d73485
Joe Sak <joe@joesak.com>
2014-04-04 23:08:07 -0400

    Refactor Report like a boss and use that new printer

--- features/step_definitions/viewing_calculated_stats_steps.rb
+++ features/step_definitions/viewing_calculated_stats_steps.rb
@@ -4,37 +4,37 @@ When(/^I display the stats report$/) do
 end
 
 Then(/^I should see that no stats are available to calculate$/) do
-  @report.message.should include "2010: There are no stats to calculate."
+  @report.body.should include "2010: There are no stats to calculate."
 end
 
 Then(/^I should see the most improved batting average$/) do
-  @report.message.should include "Most improved batting average from"
-  @report.message.should include "2009 to 2010: aardsda02"
+  @report.body.should include "Most improved batting average from"
+  @report.body.should include "2009 to 2010: aardsda02"
 end
 
 Then(/^I should see that not enough stats were found$/) do
-  @report.message.should include "2010: Not enough stats were found."
+  @report.body.should include "2010: Not enough stats were found."
 end
 
 Then(/^I should see that no eligible stats were found$/) do
-  @report.message.should include "Percentage: No eligible stats were found."
+  @report.body.should include "Percentage: No eligible stats were found."
 end
 
 Then(/^I should see the slg for the team and year specified$/) do
-  @report.message.should include "2007 OAK Slugging Percentage: "
-  @report.message.should include "\nallenbr01 - 0.356\nallenbr02 - 0.459"
+  @report.body.should include "2007 OAK Slugging Percentage: "
+  @report.body.should include "\nallenbr01 - 0.356\nallenbr02 - 0.459"
 end
 
 Then(/^I should see there was no winner$/) do
-  @report.message.should include "League: (No winner)"
+  @report.body.should include "League: (No winner)"
 end
 
 Then(/^I should see the triple crown winners$/) do
   header = "Triple Crown Eligible or Winner for"
-  @report.message.should include "#{header} 2011:"
-  @report.message.should include "\nAmerican League: alplaye02"
-  @report.message.should include "\nNational League: nlplaye02"
-  @report.message.should include "\n#{header} 2012:"
-  @report.message.should include "\nAmerican League: alplaye02"
-  @report.message.should include "\nNational League: nlplaye02"
+  @report.body.should include "#{header} 2011:"
+  @report.body.should include "\nAmerican League: alplaye02"
+  @report.body.should include "\nNational League: nlplaye02"
+  @report.body.should include "\n#{header} 2012:"
+  @report.body.should include "\nAmerican League: alplaye02"
+  @report.body.should include "\nNational League: nlplaye02"
 end
--- lib/baseball_stats/report.rb
+++ lib/baseball_stats/report.rb
@@ -1,19 +1,27 @@
 module BaseballStats
   class Report
-    attr_accessor :message
+    attr_reader :body, :printer
 
     class << self
       attr_accessor :raw_data
     end
 
     def initialize(raw_data)
-      @message  = ''
+      @printer = PlainTextPrinter.new
+      @body = "* * * * Baseball Stats Reporter * * * *\n\n"
       self.class.raw_data = clean_data_source(raw_data)
     end
 
     def display_report
-      construct_message
-      $stdout.puts message
+      reporters.each do |reporter|
+        body << reporter.header
+        begin
+          body << reporter.result << "\n\n"
+        rescue => e
+          body << e.message
+        end
+      end
+      printer.print(body)
     end
 
     private
@@ -21,54 +29,13 @@ module BaseballStats
       data.gsub("\r", "\n").gsub("\n\n", "\n").gsub(',,', ',0,')
     end
 
-    def construct_message
-      report_improved_batting_average
-      message << "\n"
-      report_slugging_percentage
-      message << "\n"
-      report_triple_crown_winners
-      message << "\n"
-    end
+    def reporters
+      triple_crown_reporter = Reporters::TripleCrownWinner
 
-    def report_improved_batting_average
-      message << improved_batting_average_reporter.header
-      begin
-        message << improved_batting_average_reporter.result
-      rescue => e
-        message << e.message
-      end
-    end
-
-    def report_slugging_percentage
-      message << "\n" << slugging_percentage_reporter.header
-      begin
-        message << slugging_percentage_reporter.result
-      rescue => e
-        message << e.message
-      end
-    end
-
-    def report_triple_crown_winners
-      [2011, 2012].each do |year|
-        message << "\n" << triple_crown_reporter(year).header
-        begin
-          message << triple_crown_reporter(year).result << "\n"
-        rescue => e
-          message << e.message
-        end
-      end
-    end
-
-    def improved_batting_average_reporter
-      Reporters::ImprovedBattingAverage.new(2010)
-    end
-
-    def slugging_percentage_reporter
-      Reporters::SluggingPercentage.new(2007, 'OAK')
-    end
-
-    def triple_crown_reporter(year)
-      Reporters::TripleCrownWinner.new(year)
+      [Reporters::ImprovedBattingAverage.new(2010),
+       Reporters::SluggingPercentage.new(2007, 'OAK'),
+       triple_crown_reporter.new(2011),
+       triple_crown_reporter.new(2012)]
     end
   end
 end
